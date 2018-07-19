25b404f92e5c442b9c3ca37c2a1bc61e70279b2e
zhangqiang <914489197@qq.com>
2018-07-19 17:18:50 +0800

    2018.07.19

--- js/style.js
+++ js/style.js
@@ -1,5 +1,8 @@
  $(document).ready(function(){
+ 	$(".icon-header").hide();
  	$(".head-div").click(function(){
  		$(".icon-header").toggle();
+ 		//$(".icon-header>ul").toggle();
+ 		//$(".icon-header>ul>li").toggle();
  	})
  })
