5f7295faeac131d0638c378f2160a8a2e9cd6214
zhangqiang <914489197@qq.com>
2018-07-30 17:52:59 +0800

    new

--- js/index.js
+++ js/index.js
@@ -3,3 +3,5 @@
             keyboard: true
         });
     });
+    
+ 
--- js/style.js
+++ js/style.js
@@ -21,3 +21,27 @@
  	
  	 
  })
+//轮播图触摸事件
+	$(document).ready(function(){
+				var startX,endX;//声明触摸的两个变量
+				var offset = 30;//声明触摸距离的变量
+				$('.carousel-inner').on('touchstart',function (e) {
+					startX= e.originalEvent.touches[0].clientX;//当触摸开始时的x坐标；
+				});
+				$('.carousel-inner').on('touchmove',function (e) {
+					endX = e.originalEvent.touches[0].clientX;//当触摸离开时的x坐标；
+				});
+				$('.carousel-inner').on('touchend',function (e) {
+					//当触摸完成时进行的事件；
+					var distance = Math.abs(startX - endX);//不论正负，取值为正值；
+					if (distance > offset){
+					    if(startX > endX){
+					        $('#myCarousel').carousel('next');//当开始的坐标大于结束的坐标时，滑动到下一附图
+					    }else{
+					      	$('#myCarousel').carousel('prev');//当开始的坐标小于结束的坐标时，滑动到上一附图
+ 
+					    }
+					        	
+					}
+				});	
+			});
