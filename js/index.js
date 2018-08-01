aead1076b843c718d8dd30a073249500fbb33d3a
zhangqiang <914489197@qq.com>
2018-08-01 18:37:44 +0800

    new

--- js/index.js
+++ js/index.js
@@ -1,7 +1,48 @@
   $(function(){
+  	//模态框
          $("#myModal").modal({
             keyboard: true
         });
+        //模态框
+        //shujujiaohu
+       
+         $.ajax({
+        url:"http://www.dianyitai.cn/api/homes",
+        type:"post",
+        /*xhrFieds:{
+        	widthCredentials:true
+        },*/
+        data:"",
+        /*预期服务器端返回的数据类型，假设现在跨域了，就改成jsonp 就可以了 */
+        dataType:"json",
+        success:function(data){
+            console.log(data);
+            
+            
+                    $('.content1').empty();   //清空resText里面的所有内容
+                    var html = '';
+                    $.each(data.data, function(key, list){
+                         
+                        
+                        html +=' ';
+                      
+
+                    });
+                    $('.content1').html(html);             
+
+        }
+    })
+       
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     });
     
  
